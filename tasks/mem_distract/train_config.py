3a5b699d8b437219e3886cad8aaa31b813f1f6c8
Jayram Thathachar <jayram@us.ibm.com>
2018-01-24 22:11:18 -0800

    First distract runs

--- tasks/mem_distract/train_config.py
+++ tasks/mem_distract/train_config.py
@@ -11,21 +11,19 @@ def train_config():
     report_interval = REPORT_INTERVAL
     epochs = 200000
 
-    N_train = 256
+    N_train = 60
     train_batch_size = 1
-    train_min_num_seq = 6
-    train_max_num_seq = 10
-    train_avg_len = 7
+    train_min_num_seq = 3
+    train_max_num_seq = 4
+    train_avg_len = 5
 
 
 @ex.config
 def test_config():
-    N_test = 400
-    test_batch_size = 32
-    test_min_num_seq = 10
-    test_max_num_seq = 15
-    test_avg_len = 12
+    N_test = 180
+    test_batch_size = 32 
+    test_min_num_seq = 6
+    test_max_num_seq = 8
+    test_avg_len = 10
 
 # end change ---
-
-
