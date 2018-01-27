2b45b3e611f0288542119bdba75cdfc884b4de46
Jayram Thathachar <jayram@us.ibm.com>
2018-01-26 21:20:40 -0800

    version where encoder=decoder control channel is first

--- tasks/encoder/build.py
+++ tasks/encoder/build.py
@@ -21,7 +21,7 @@ def model_config():
 @ex.capture
 def build_ntm(element_size, tm_state_units, is_cam, num_shift, N, M):
     in_dim = element_size + 1
-    out_dim = element_size
+    out_dim = element_size + 1
     aux_in_dim = 1
     ret_seq = True
     ntm = NTMSolver(in_dim, out_dim, aux_in_dim, tm_state_units,
@@ -39,11 +39,9 @@ def build_data_gen(ntm, batch_size, min_len, max_len, bias, element_size, _rnd):
     while True:
         encoder_length = _rnd.randint(low=min_len, high=max_len + 1)
         seq = _rnd.binomial(1, bias, (batch_size, encoder_length, element_size))
-        values = np.zeros((seq.shape[0], seq.shape[1], 1))
-        encoder_input = np.append(seq, values, axis=2)
-        encoder_input = np.insert(encoder_input, 0, 0, axis=1)
-        encoder_input[:, 0, -1] = 1
-        target = seq
+        encoder_input = np.insert(seq, 0, 0, axis=1)
+        encoder_input = np.insert(encoder_input, 0, 0, axis=2)
+        target = encoder_input
         aux_seq = np.ones((batch_size, target.shape[1], aux_in_dim)) * 0.5
         inputs = [encoder_input, aux_seq]
         yield inputs, target, encoder_length
