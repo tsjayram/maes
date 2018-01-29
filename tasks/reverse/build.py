b995e245bacbc96781d4bea562df0d6681af514d
Jayram Thathachar <jayram@us.ibm.com>
2018-01-28 22:08:56 -0800

    stuff

--- tasks/reverse/build.py
+++ tasks/reverse/build.py
@@ -37,14 +37,15 @@ def build_data_gen(ntm, batch_size, min_len, max_len, bias, element_size, _rnd):
 
     aux_in_dim = 1
     while True:
-        encoder_length = _rnd.randint(low=min_len, high=max_len + 1)
-        seq = _rnd.binomial(1, bias, (batch_size, encoder_length, element_size))
+        target_length = _rnd.randint(low=min_len, high=max_len + 1)
+        seq = _rnd.binomial(1, bias, (batch_size, target_length, element_size))
         encoder_input = np.insert(seq, 0, 0, axis=1)
         encoder_input = np.insert(encoder_input, 0, 0, axis=2)
         encoder_input[:, 0, 0] = 1
+        
         target = np.flip(seq, axis=1)
         aux_seq = np.ones((batch_size, target.shape[1], aux_in_dim)) * 0.5
         inputs = [encoder_input, aux_seq]
-        yield inputs, target, encoder_length
+        yield inputs, target, target_length
 
 # end change ---
