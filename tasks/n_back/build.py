e166133f0ef932e71de1bf58c13698428d74619e
Jayram Thathachar <jayram@us.ibm.com>
2018-02-04 22:46:10 -0800

    Start of training for n_back with bidirectional encoder

---
+++ tasks/n_back/build.py
@@ -0,0 +1,53 @@
+import numpy as np
+from sacred import Experiment
+
+from tasks.solver import NTMSolver
+
+# change below based on task ----
+TASK_NAME = 'n_back'
+ex = Experiment(TASK_NAME)
+LOG_ROOT = '../../logs/'
+
+
+@ex.config
+def model_config():
+    element_size = 8
+    tm_state_units = 3
+    num_shift = 3
+    is_cam = False
+    M = 10
+
+
+@ex.capture
+def build_ntm(element_size, tm_state_units, is_cam, num_shift, N, M):
+    in_dim = element_size + 1
+    out_dim = 1
+    aux_in_dim = element_size
+    ret_seq = True
+    ntm = NTMSolver(in_dim, out_dim, aux_in_dim, tm_state_units,
+                    ret_seq, is_cam, num_shift, N, M)
+    return ntm
+
+
+@ex.capture
+def build_data_gen(ntm, batch_size, min_len, max_len, bias, element_size, _rnd):
+    yield ntm.init_state(batch_size)
+
+    while True:
+        seq_length = _rnd.randint(low=min_len, high=max_len + 1)
+        seq = _rnd.binomial(1, bias, (batch_size, seq_length, element_size))
+        encoder_input = np.insert(seq, 0, 0, axis=1)
+        encoder_input = np.insert(encoder_input, 0, 0, axis=2)
+        encoder_input[:, 0, 0] = 1
+
+        mask = _rnd.binomial(1, 0.5, (batch_size, seq_length))
+        input_xor = _rnd.binomial(1, 0.5, seq.shape)
+        input_xor = input_xor * mask[:, :, np.newaxis]
+        aux_seq = np.logical_xor(seq, input_xor).astype(float)
+
+        target = np.any(input_xor, axis=2, keepdims=True)
+
+        inputs = [encoder_input, aux_seq]
+        yield inputs, target, seq_length
+
+# end change ---
---
+++ tasks/n_back/train_config.py
@@ -0,0 +1,69 @@
+import numpy as np
+import h5py
+from tasks.utils import train_status_gen
+
+# change below based on task ----
+from tasks.n_back.build import ex, LOG_ROOT
+from tasks.n_back.build import build_ntm, build_data_gen
+
+ENCODER_FREEZE_TIME = '2018-02-02__05_55_02_PM'
+ENCODER_EPOCH = 43100
+
+RANDOM_SEED = 12345
+REPORT_INTERVAL = 100
+
+
+@ex.config
+def train_test_config():
+    seed = RANDOM_SEED
+    epochs = 50000
+    N_train = 30
+    N_test = 80
+    train_batch_size = 16
+    test_batch_size = 64
+    train_min_len = 3
+    train_max_len = 20
+    test_len = 64
+    bias = 0.5
+    report_interval = REPORT_INTERVAL
+
+# end change ---
+
+
+@ex.capture
+def get_train_status(train_max_len, report_interval):
+    threshold = (train_max_len * 3) // 4
+    return train_status_gen(threshold, report_interval)
+
+
+@ex.config
+def encoder_weights():
+    use_frozen_wts = True
+    encoder_freeze_wts_file = LOG_ROOT + 'encode/' + ENCODER_FREEZE_TIME + '/model_weights.hdf5'
+    encoder_epoch = ENCODER_EPOCH
+
+
+@ex.capture
+def freeze_encoder(ntm, use_frozen_wts, encoder_freeze_wts_file, encoder_epoch):
+    if use_frozen_wts:
+        epoch_key = 'epoch_{:05d}'.format(encoder_epoch)
+        with h5py.File(encoder_freeze_wts_file, 'r') as f:
+            print('In ', epoch_key)
+            grp = f[epoch_key]
+            weights = [np.array(grp[name]) for name in grp if 'Encoder' in name]
+            ntm.encoder_freeze_weights(weights)
+
+
+@ex.capture
+def build_train(N_train, train_batch_size, train_min_len, train_max_len, bias):
+    ntm = build_ntm(N=N_train)
+    freeze_encoder(ntm)
+    data_gen = build_data_gen(ntm, train_batch_size, train_min_len, train_max_len, bias)
+    return ntm, data_gen
+
+
+@ex.capture
+def build_test(N_test, test_batch_size, test_len, bias):
+    ntm = build_ntm(N=N_test)
+    data_gen = build_data_gen(ntm, test_batch_size, test_len, test_len, bias)
+    return ntm, data_gen
